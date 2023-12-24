2c201ccfe473040456cac7236b373c55f6171a81
alex <alex@pdp7.net>
2023-12-24 13:48:36 +0100

    Add support to disable check, capture output

--- spl/utils.py
+++ spl/utils.py
@@ -6,6 +6,9 @@ import subprocess
 logger = logging.getLogger(__name__)
 
 
-def run_command(command):
+def run_command(command, check=True, capture_stdout=False):
     logger.info("running %(command)s", {"command": shlex.join(command)})
-    return subprocess.run(command, check=True)
+    kwargs = {}
+    if capture_stdout:
+        kwargs["stdout"] = subprocess.PIPE
+    return subprocess.run(command, check=check, encoding="utf8", **kwargs)
